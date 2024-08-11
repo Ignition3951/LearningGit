78495ffaac1b517d86484f89b6586e7c9078cd0e
Utkarsh <ignition3951@gmail.com>
2024-08-11 21:40:48 +0530

    Testing changes

--- LearnGit/src/com/utk/Main.java
+++ LearnGit/src/com/utk/Main.java
@@ -5,13 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Calling main method");
 		
-		System.out.println("Hotfix Applied!!!!!!");
-		
-		System.out.println("Commit for issue 13!!!!!!!");
-		
-		System.out.println("Commit for issue 13!!!!!!!");
-		
-		System.out.println("Testing branch changes!!!!!!!");
+		System.out.println("Changing the testing branch!!");
 	}
 
 }
